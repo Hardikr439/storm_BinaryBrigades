53b3c3aed9f5c324e2ed452265216d89f8fc5ef6
AtharvaN29 <narvekaratharva29@gmail.com>
2023-10-05 01:25:30 +0530

    Review & Recognition

--- src/landingPage.jsx
+++ src/landingPage.jsx
@@ -96,29 +96,115 @@ function LandingPage() {
           the higher authorities
         </h2>
       </div>
-      <div className='d-flex flex-column flex-md-row p-4 gap-4 py-md-5 align-items-center justify-content-center'>
-        <div className='list-group list-group-checkable d-grid gap-2 border-0'>
-          <button type='button' className='w-100 btn btn-lg btn-warning m-3'>
-            <h2 className='text-center'>CEO</h2>
-            <h4 className='text-center'>Mrs.Kriya trivedi</h4>
-          </button>
+      <div class="form-floating mb-3 mx-5">
+      <label for="formId1">Name of the Authority</label>
+        <input
+          type="text"
+          class="form-control" name="formId1" id="formId1" placeholder=""/>
+      </div>
+      <div class="form-floating mb-3 mx-5">
+      <label for="formId1">Grievance</label>
+        <input
+          type="text"
+          class="form-control" name="formId1" id="formId1" placeholder=""/>
+      </div>
+      <div>
+         <button type="submit" class="btn btn-primary mx-5">Submit</button>
+      </div>
 
-          <button type='button' className='w-100 btn btn-lg btn-warning m-3'>
-            <h2 className='text-center'>HR</h2>
-            <h4 className='text-center'>Mr.Shyam Rajput</h4>
-          </button>
+      <div className='text-center'>
+        <h2 className='nav-link px-2 text-success' id='myTitle'>
+          Update about the work done in this week
+        </h2>
+      </div>
+      <div class="form-floating mb-3 mx-5">
+      <label for="formId1">Name of the employee</label>
+        <input
+          type="text"
+          class="form-control" name="formId1" id="formId1" placeholder=""/>
+      </div>
+      <div class="form-floating mb-3 mx-5">
+      <label for="formId1">No.of project submitted</label>
+        <input
+          type="text"
+          class="form-control" name="formId1" id="formId1" placeholder=""/>
+      </div>
+      <div class="btn-group btn-group-toggle" data-toggle="buttons">
+        <h4 class="mx-5">Project submission time:</h4>
+        <label class="btn btn-primary mx-2">
+          <input type="radio" name="options" id="option1" autocomplete="off" checked/>On Time
+        </label>
+        <label class="btn btn-primary mx-2">
+          <input type="radio" name="options" id="option2" autocomplete="off"/> Late
+        </label>
+      </div>
+      <br></br>
+      <br></br>
+      <div>
+         <button type="submit" class="btn btn-primary mx-5">Submit</button>
+      </div>
 
-          <button type='button' className='w-100 btn btn-lg btn-warning m-3'>
-            <h2 className='text-center'>Project Manager</h2>
-            <h4 className='text-center'>Mr.aayush Shah</h4>
-          </button>
-
-          <button type='button' className='w-100 btn btn-lg btn-warning m-3'>
-            <h2 className='text-center'>Employee</h2>
-            <h4 className='text-center'>Any employee working in our firm</h4>
-          </button>
+      <div className='text-center mx-5' id="Review">
+        <h2 className='nav-link px-2 text-success' id='myTitle'>
+           Read what the employers think about your work!
+        </h2>
+        <div className='col' id='myCard1'>
+          <div className='card mb-4 rounded-3 border-primary' id='ReviewCard'>
+            <div className='card-header py-3 text-bg-primary border-primary'>
+              <h4 className='my-0 fw-normal'>The CEO</h4>
+            </div>
+            <div className='card-body'>
+              <h1 className='card-title pricing-card-title'>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur itaque laborum omnis recusandae ducimus culpa ipsum odit suscipit et ipsa!
+              </h1>
+            </div>
+          </div>
+        </div>
+        <div className='col' id='myCard1'>
+          <div className='card mb-4 rounded-3 border-primary' id='ReviewCard'>
+            <div className='card-header py-3 text-bg-primary border-primary'>
+              <h4 className='my-0 fw-normal'>Project Manager</h4>
+            </div>
+            <div className='card-body'>
+              <h1 className='card-title pricing-card-title'>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur itaque laborum omnis recusandae ducimus culpa ipsum odit suscipit et ipsa!
+              </h1>
+            </div>
+          </div>
         </div>
       </div>
+
+
+      <div className='text-center'>
+        <h2 className='nav-link px-2 text-success' id='myTitle'>
+           Check out the best employees of the month!
+        </h2>
+        <div id="BestEmp">
+        <div className='col' id='myCard1'>
+          <div className='card mb-4 rounded-3 border-primary' id='inMyCard'>
+            <div className='card-header py-3 text-bg-primary border-primary'>
+              <h4 className='my-0 fw-normal'>Mr.Ashwin</h4>
+            </div>
+          </div>
+        </div>
+        <div className='col' id='myCard1'>
+          <div className='card mb-4 rounded-3 border-primary' id='inMyCard'>
+            <div className='card-header py-3 text-bg-primary border-primary'>
+              <h4 className='my-0 fw-normal'>Mr.Williams</h4>
+            </div>
+          </div>
+        </div>
+        <div className='col' id='myCard1'>
+          <div className='card mb-4 rounded-3 border-primary' id='inMyCard'>
+            <div className='card-header py-3 text-bg-primary border-primary'>
+              <h4 className='my-0 fw-normal'>Mrs.Sands</h4>
+            </div>
+          </div>
+        </div>
+        </div>
+        
+      </div>
+      
     </>
   )
 }
